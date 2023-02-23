bfeae2c9f9e05b82136251b2564c88fa93af490a
Federico Bandini <fede_2007@hotmail.com>
2023-02-23 11:48:08 -0300

    Se modifica barra de navegacion y prueba de background

--- Proyecto/HTML/JS/javas.js
+++ Proyecto/HTML/JS/javas.js
@@ -1,3 +1,8 @@
-document.getElementById(boton1).onclick = function(fede){
-   fede = console.log("Hola Mundo!!!")
+//document.getElementById(boton1).onclick = function(fede){
+//   fede = console.log("Hola Mundo!!!")
+//}
+var boton = document.getElementById("boton1");
+
+boton.onclick = function() {
+   alert(this.innerHTML);
 }
