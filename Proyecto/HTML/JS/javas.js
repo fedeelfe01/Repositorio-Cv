fa52b1eab82b102727c9f62d2eeb7f9e6178446c
Federico Bandini <fede_2007@hotmail.com>
2023-02-24 11:56:30 -0300

    agrega js y modifican botones ´ver´

--- Proyecto/HTML/JS/javas.js
+++ Proyecto/HTML/JS/javas.js
@@ -1,8 +1,5 @@
-//document.getElementById(boton1).onclick = function(fede){
-//   fede = console.log("Hola Mundo!!!")
-//}
-var boton = document.getElementById("boton1");
 
-boton.onclick = function() {
-   alert(this.innerHTML);
+document.getElementById('boton1').onclick = function () {
+   console.log("Funcionaaa");
+   document.getElementById('all.nombre').innerHTML = "Federico Ezequiel Bandini";
 }
