6758eec4fed30712b09b575409684b8c074115d9
Federico Bandini <fede_2007@hotmail.com>
2023-02-27 19:56:50 -0300

    Se coloca imagen final y se completan agregan sentencias en JVs

--- Proyecto/HTML/JS/javas.js
+++ Proyecto/HTML/JS/javas.js
@@ -8,41 +8,89 @@ var tel=0;
 var mai=0; }
 
 document.getElementById('boton1').onclick = function () {
-   console.log("Nombre");
-   if (nom ==0){
+      if (nom ==0){
    document.getElementById('all.nombre').innerHTML = "Federico Ezequiel Bandini";
-   nom+1;
+   nom=1;
 }
 else{
-   document.getElementById('all.nombre').innerHTML = '';
+   document.getElementById('all.nombre').innerHTML = "";
+   nom=0;
 }
 }
 
 document.getElementById('boton2').onclick = function () {
-   console.log("Edad");
-   document.getElementById('edad2').innerHTML = "34 Años";
+   if (eda ==0){
+     document.getElementById('edad2').innerHTML = "34 Años";
+     eda=1;
+   }
+   else{
+      document.getElementById('edad2').innerHTML = "";
+      eda=0;
+   }
+
 }
 document.getElementById('boton3').onclick = function () {
-   console.log("Fecha");
+   if (fech ==0){
    document.getElementById('fecha2').innerHTML = "22 de Septiembre de 1988";
+   fech=1;
 }
+else{
+   document.getElementById('fecha2').innerHTML = "";
+   fech=0;
+}
+}
+
 document.getElementById('boton4').onclick = function () {
-   console.log("Nacionalidad");
-   document.getElementById('nacion2').innerHTML = "Argentino";
-}
+   if (nac ==0){
+      document.getElementById('nacion2').innerHTML = "Argentino";
+      nac=1;
+   }
+   else{
+      document.getElementById('nacion2').innerHTML = "";
+      nac=0;
+   }
+   }
+
 document.getElementById('boton5').onclick = function () {
-   console.log("DNI");
-   document.getElementById('dni2').innerHTML = "34.076.573";
-}
+   if (dn ==0){
+      document.getElementById('dni2').innerHTML = "34.076.573";
+      dn=1;
+   }
+   else{
+      document.getElementById('dni2').innerHTML = "";
+      dn=0;
+   }
+   }
+
 document.getElementById('boton6').onclick = function () {
-   console.log("Direccion");
-   document.getElementById('dire2').innerHTML = "Terrada 5462, Isidro Casanova";
+   if (dir ==0){
+      document.getElementById('dire2').innerHTML = "Terrada 5462, Isidro Casanova";
+      dir=1;
+   }
+   else{
+      document.getElementById('dire2').innerHTML = "";
+      dir=0;
+   }
 }
 document.getElementById('boton7').onclick = function () {
-   console.log("Telefono");
-   document.getElementById('celu2').innerHTML = "15-3007-8248";
+   
+   if (tel ==0){
+      document.getElementById('celu2').innerHTML = "15-3007-8248";
+      tel=1;
+   }
+   else{
+      document.getElementById('celu2').innerHTML = "";
+      tel=0;
+   }
 }
 document.getElementById('boton8').onclick = function () {
-   console.log("Mail");
-   document.getElementById('mail2').innerHTML = "Fede_2007@hotmail.com";
+   
+   if (mai ==0){
+      document.getElementById('mail2').innerHTML = "Fede_2007@hotmail.com";
+      mai=1;
+   }
+   else{
+      document.getElementById('mail2').innerHTML = "";
+      mai=0;
+   }
 }
